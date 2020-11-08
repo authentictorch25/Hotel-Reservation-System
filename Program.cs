665b6968f1184cf3904ef7328c8007abbfe6bea3
authentictorch25 <64377971+authentictorch25@users.noreply.github.com>
2020-11-08 12:44:53 +0530

    [Akash] Created Program Hotel reservation System

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HotelResevationSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Hotel Reservation Sysyem");
+        }
+    }
+}
