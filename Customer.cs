69a6a42bd1019af7dc62eddd59c3242b94d315d1
authentictorch25 <64377971+authentictorch25@users.noreply.github.com>
2020-11-08 14:40:22 +0530

    [Akash] Added the program to incorporate customer type

---
+++ Customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelResevationSystem
+{
+    public enum Customer
+    {
+        REGULAR_CUSTOMER,
+        REWARDS_CUSTOMER
+    }
+}
