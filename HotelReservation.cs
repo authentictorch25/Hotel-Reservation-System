4468b701e50cce935583ff7e14482cc44d9e154b
authentictorch25 <64377971+authentictorch25@users.noreply.github.com>
2020-11-08 13:17:14 +0530

    [Akash] UC2 Added program to get cheapest total stay expense

--- HotelReservation.cs
+++ HotelReservation.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace HotelResevationSystem
@@ -25,6 +26,35 @@ namespace HotelResevationSystem
                 Hotel hotel = new Hotel(name, regularRate);
                 //Adding the hotel to dictionary
                 HotelBook.Add(name, hotel);
+            }
+        }
+        public static void GetCheapestHotel()
+        {
+            try
+            {
+                //Entering the checkin date
+                Console.WriteLine("Enter the checkin date, Format DDMMYYYY");
+                //Entering the checkout date
+                DateTime checkIn = DateTime.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the checkout date, Format DDMMYYYY");
+                DateTime checkOut = DateTime.Parse(Console.ReadLine());
+                //Count of days stayed (suppose a person stays from 2/10-3/10, Substraction will give 1 but actual stay is 2 days hence +1)
+                int numOfDays = (checkOut - checkIn).Days +1;
+                Dictionary<string, int> rateRecords = new Dictionary<string, int>();
+                //Adding a dictionary to get the rates and name of the hotel
+                foreach (var v in HotelBook)
+                {
+                    int rate = v.Value.regularRate * numOfDays;
+                    rateRecords.Add(v.Value.hotelName, rate);
+                }
+                //Getting the detail of the hotel and the total rate
+                var hotelDetails = rateRecords.OrderBy(kvp => kvp.Value).First();
+                Console.WriteLine($"{hotelDetails.Key},TotalRate:{hotelDetails.Value}");
+            }
+            //In case any exception Arises
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
             }
         }
     }
--- Program.cs
+++ Program.cs
@@ -6,10 +6,14 @@ namespace HotelResevationSystem
     {
         static void Main(string[] args)
         {
+            //UC1
             Console.WriteLine("Welcome to Hotel Reservation Sysyem");
             HotelReservation.AddHotel("Lakewood", 110);
             HotelReservation.AddHotel("Bridgewood", 150);
             HotelReservation.AddHotel("Ridgewood", 220);
+            
+            //UC2
+            HotelReservation.GetCheapestHotel();
             Console.ReadKey();
         }
     }
